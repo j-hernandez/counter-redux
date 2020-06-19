a922b3e07a6db2d3400e8f35f80018adf8cde43d
James Hernandez <james@j-hernandez.com>
2020-06-18 20:11:45 -0400

    feat(increment): adds increment function

---
+++ src/components/Counter.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    increment = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    decrement = () => {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Counter</h2>
+                <div>
+                    <button>-</button>
+                    <span>{this.state.count}</span>
+                    <button onClick={this.increment}>+</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Counter from './components/Counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
